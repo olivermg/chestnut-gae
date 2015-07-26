4ff6fbd24c95d5242abe48a5689925b77e34d6bd
Oliver Wegner <void1976@gmail.com>
2015-07-26 12:20:05 +0200

    our own reactive component

--- src/cljs/chestnut_gae/core.cljs
+++ src/cljs/chestnut_gae/core.cljs
@@ -4,24 +4,39 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(defonce app-state (atom {:text "Hello Chestnut!"
+                          :toggle-state {:checked false}}))
+
+(defn checkbox-view
+  [state owner {:keys [text key]}]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/input #js {:type "checkbox" :checked (key state)
+                      :onClick (fn [] (om/transact! state key not))}
+                 text))))
+
+(defn toggle-view
+  [state owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div nil
+               (om/build checkbox-view state {:opts {:text "toggle 1" :key :checked}})
+               (dom/br nil)
+               (om/build checkbox-view state {:opts {:text "toggle 2" :key :checked}})))))
+
+(defn app-view
+  [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (om/build toggle-view (:toggle-state app)))))
 
 (defn main []
-
   (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/h1 nil (:text app)))))
-    app-state
-    {:target (. js/document (getElementById "app"))})
-
-  (om/root
-   (fn [data owner]
-     (om/component
-      (dom/h2 nil (:text data))))
+   app-view
    app-state
-   {:target (. js/document (getElementById "app1"))}))
+   {:target (.getElementById js/document "toggle")}))
 
 (main)
